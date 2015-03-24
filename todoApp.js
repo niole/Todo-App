77d4c75da974b459d7a2d40831017a6d586cd816
Niole Nelson <niolenelson@gmail.com>
2015-03-24 14:07:55 -0400

    Centered DOM elements, deleted X2 button, added datepicker, added dropdown delete messages

--- todoApp.js
+++ todoApp.js
@@ -1,14 +1,18 @@
+"use strict";
+/*global React*/
+
 var TodoApp = React.createClass({
   getInitialState: function() { return todolist; },
   render: function() {
     return (
-      <div className='container-fluid'>
-        <h1>make a todo list</h1>
-        <button className="btn btn-primary" onClick={this._handleSort}>sort!</button><br/><br/>
-        <button className="btn" onClick={this._handleDouble}>x2</button>
-        <Todoadder onAdd={this._handleAdd} />
+      <div className='container-fluid' style={{'textAlign':'center'}}>
+        <div className="page-header">
+        <h1>Todo Lists..<small>...make one!</small></h1>
+        </div>
         <Displaytodos todos={this.state.todos} filterText={this.state.filterText} onDelete={this._handleDelete}/>
-        <FilterForm todos={this.state.todos} onFilter={this._handleFilter}/>
+        <Todoadder onAdd={this._handleAdd} />
+        <FilterForm todos={this.state.todos} onFilter={this._handleFilter}/><br/>
+        <button className="btn btn-primary" onClick={this._handleSort}>sort by deadline</button>
       </div>
     );
   },
@@ -33,47 +37,51 @@ var TodoApp = React.createClass({
     }
     return this.setState(todolist);
   },
-  _handleDouble: function() {
-    todolist.todos = _.map(todolist.todos, function(e) {return e+e;});
-    return this.setState(todolist);
-  }
 });
 
 var FilterForm = React.createClass( {
   propTypes: {
-  todos: React.PropTypes.array.isRequired,
-  onFilter: React.PropTypes.func.isRequired,
+    todos: React.PropTypes.array.isRequired,
+    onFilter: React.PropTypes.func.isRequired,
   },
   render: function() {
     return (
       <form onSubmit={this._handleSubmit}>
-      filter: <input type="text" name="filter"  placeholder=""/>
+        <input type="text" name="filter"  placeholder="check if it's in the list"/>
       </form>
     );
   },
   _handleSubmit: function(event) {
     event.preventDefault();
-    event.isDefaultPrevented();
     var tomatch = $(event.target).find("input[name=filter]").val();
     return this.props.onFilter(tomatch);
     }
   });
 
+//http://materializecss.com/forms.html
 var Todoadder = React.createClass( {
   propTypes: {
     onAdd: React.PropTypes.func.isRequired,
   },
+  componentDidMount: function() {
+    $('#datepicker').datepicker();
+  },
   render: function() {
     return (
-      <form id="todoForm" onSubmit={this._handleSubmit}>
-      add: <input type="text" name="todo" placeholder=""/>
-      </form>
+      <div>
+        <form id="todoForm" onSubmit={this._handleSubmit}>
+          <input type="text" name="todo" placeholder="what's on your todo list?"/>
+          <input id="datepicker" placeholder="set a deadline"/>
+        </form>
+      </div>
     );
   },
   _handleSubmit: function(event) {
     event.preventDefault();
-    event.isDefaultPrevented();
-    this.props.onAdd($(event.target).find("input[name=todo]").val());
+    var newTodo = $(event.target).find("input[name=todo]").val();
+    if (newTodo !== '') {
+      this.props.onAdd(newTodo);
+    }
   }
 });
 var todolist = {
@@ -99,11 +107,23 @@ var Displaytodos = React.createClass( {
       return elt.indexOf(filterText) > -1;
     });
 
-    listedtodos = [];
+    var listedtodos = [];
     for (var key in todos) {
-      listedtodos.push(<li className="list-group-item" id={key} onClick={this._handleItemClick}>{todos[key]}</li>);
+      listedtodos.push(<tr >
+                        <td id="todoitem" className="dropdown"><a href="#" className="dropdown-toggle" data-toggle="dropdown">{todos[key]}</a>
+                         <ul className="dropdown-menu">
+                          <li><a id={key} href="#" onClick={this._handleItemClick}>delete</a></li>
+                         </ul>
+                        </td>
+                      </tr>);
     }
-    return <ul className="list-group">{listedtodos}</ul>;
+    return (
+        <table className="table">
+          <tbody>
+            {listedtodos}
+          </tbody>
+        </table>
+    );
   },
   _handleItemClick: function(event) {
     var key = event.target.id;
